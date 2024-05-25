5cf7e20ce0f713e5602d24fa4ef5c5d59a09e826
睿 <122418084+Rui01sir@users.noreply.github.com>
2024-05-26 03:12:15 +0800

    Update Page404.js

--- src/components/Page404.js
+++ src/components/Page404.js
@@ -5,7 +5,7 @@ function Page404() {
   return (
     <div>
       <div className="content-2">
-        <img src="./img/icon-large/icon-large-05.svg" alt="404"></img>
+        <img src={`${process.env.PUBLIC_URL}./img/icon-large/icon-large-05.svg"`} alt="404"></img>
         <h2>這個頁面即將上線 ！</h2>
         <p>我們的網頁正在進行調整，很快就會與大家見面～</p>
         <Link to="/"><button  className="btn-4" >
